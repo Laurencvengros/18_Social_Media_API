ea8037e9097c63b0086c681f18223378da5916b2
Lauren <l.cvengros@icloud.com>
2022-11-16 11:49:11 -0500

    added reactionSchema to thought model in thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,29 +1,61 @@
 const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max_length: 280
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh: mm a')
+
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    },
+    
+); 
 
 const thoughtSchema = new Schema(
     {
-     thoughtText: {
-        type: String,
-        required: true,
-        min_length: 1,
-        max_length: 280,
-     },
-     createdAt: {
-        type: Date,
-        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a'),
-     },
-     username: {
-        type: String,
-        required: true,
-     },
-     reactions: [reactionSchema]
+        thoughtText: {
+            type: String,
+            required: true,
+            min_length: 1,
+            max_length: 280,
+        },
+         createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a'),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema]
     },
     {
-      toJSON: {
-        virtuals: true,
-        getters: true,
-      },
-      id: false,
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
     }
 );
 
